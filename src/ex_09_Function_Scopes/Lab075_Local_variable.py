8bf63c6d79ba5568b7c5604cdd3ce6bdd49fb529
Girish <gbsawant7378@gmail.com>
2026-01-27 19:53:18 +0530

    Python

---
+++ src/ex_09_Function_Scopes/Lab075_Local_variable.py
@@ -0,0 +1,10 @@
+pb_global_b = 12
+
+def my_function():
+    pb_a = 10
+    print(pb_a)
+    print(pb_global_b)
+
+# print(pb_a)
+print(pb_global_b)
+my_function()
---
+++ src/ex_09_Function_Scopes/Lab076.py
@@ -0,0 +1,10 @@
+public_toilet = "PB"
+
+def home():
+    private_toilet = "PT"
+    print(public_toilet)
+    print(private_toilet)
+
+def stranger():
+    print(public_toilet)
+    # print(private_toilet)
